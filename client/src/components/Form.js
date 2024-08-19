e8c5adeafd8d7b734a790d9e0685841f16f10498
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-08-19 14:19:28 +0530

    Update Form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -43,10 +43,10 @@ function Form() {
   return (
     <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-4 p-4 sm:w-[500px] sm:p-8">
       
-      <input type="text" value={roomId} onChange={(e) => setRoomId(e.target.value)} className="w-full rounded-md border border-gray-500 bg-darkHover px-3 py-3 focus:outline-none text-white" placeholder="Username" onKeyUp={handleInputEnter}/>
-      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full rounded-md border border-gray-500 bg-darkHover px-3 py-3 focus:outline-none text-white" placeholder="Room ID" onKeyUp={handleInputEnter} />
+      <input type="text" value={roomId} onChange={(e) => setRoomId(e.target.value)} className="w-full rounded-md border border-gray-500 bg-darkHover px-3 py-3 focus:outline-none text-white" placeholder="Room ID" onKeyUp={handleInputEnter}/>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full rounded-md border border-gray-500 bg-darkHover px-3 py-3 focus:outline-none text-white" placeholder="Username" onKeyUp={handleInputEnter} />
 
-      <button className="mt-2 w-full rounded-md bg-[#BA68C8] px-8 py-3 text-lg font-semibold text-white" onClick={joinRoom}>
+      <button className="mt-2 w-full rounded-md bg-[#38E078] px-8 py-3 text-lg font-semibold text-white" onClick={joinRoom}>
         JOIN
       </button>      
       
